930d67471104875d859003e0201b0119776f7f8a
Jai <jai.chhatre@gmail.com>
2019-04-17 23:17:05 -0700

    reorganized weapon subclasses

--- src/IceBullet.java
+++ src/IceBullet.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class IceBullet extends Actor implements Weapon
+public class IceBullet extends Weapon
 {
     /**
      * Act - do whatever the IceBullet wants to do. This method is called whenever
