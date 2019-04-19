c65e7ad5e143b8ece5f83d5b85f479e580aabb45
Jai <jai.chhatre@gmail.com>
2019-04-18 20:31:52 -0700

    interface classes for command pattern

---
+++ src/IBonusReceiver.java
@@ -0,0 +1,10 @@
+/**
+ * Write a description of class IBonusReceiver here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface IBonusReceiver  
+{
+    public void doAction();
+}
