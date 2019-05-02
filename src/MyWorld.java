a7f9f2978cd9ef10bdfbef95ff57a2ea008b36d9
Mrunali Sanjay Khandat <monakhandat@gmail.com>
2019-05-01 23:56:35 -0700

    Commit

--- src/MyWorld.java
+++ src/MyWorld.java
@@ -65,7 +65,7 @@ public class MyWorld extends World
         bg.scale(getWidth(), getHeight());
         setBackground(bg);
         addObject(castle ,370,170);
-        addObject(player2, 370, 275);
+        addObject(player, 370, 275);
         addObject(enemy , 370 ,540);
         addObject(score , 740, 200);
         addObject(new Clouds(),340, 50);
@@ -89,6 +89,7 @@ public class MyWorld extends World
                      anotherEnemy.setHealth(200);
                      iceBulletSpeed = 5;
                      break;
+                     
                      
        
         }
