586396e4745bd73279dfebff5a85dfa6b7814f0b
Mugdha Wadikar <mugdha.wadikar@sjsu.edu>
2019-05-02 01:04:26 -0700

    Created class enemyScoreboard to show enemy count in score board

---
+++ src/EnemyScoreboard.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class EnemyScoreboard here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class EnemyScoreboard extends Actor
+{
+    /**
+     * Act - do whatever the EnemyScoreboard wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
