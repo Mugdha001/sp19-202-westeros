547c11c1dac11d2f1f9864425fbcd0f7eeec5fea
Mugdha Wadikar <mugdha.wadikar@sjsu.edu>
2019-05-01 18:51:32 -0700

    rename a few files

--- src/firesplat.java
+++ src/firesplat.java
@@ -1,14 +1,14 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class firesplat here.
+ * Write a description of class Firesplat here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
 public class Firesplat extends Actor
 {
-    /**
+        /**
      * Act - do whatever the firesplat wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
