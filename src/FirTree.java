0800ee30d0248dcac013da4f460e54a1c8410f1e
Jai <jai.chhatre@gmail.com>
2019-05-02 00:31:46 -0700

    flyweight pattern classes

---
+++ src/FirTree.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class FirTree here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FirTree extends Actor
+{
+    /**
+     * Act - do whatever the FirTree wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
---
+++ src/ITreeInterface.java
@@ -0,0 +1,10 @@
+/**
+ * Write a description of class ITreeInterface here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface ITreeInterface  
+{
+    public void setSize(int size);
+}
