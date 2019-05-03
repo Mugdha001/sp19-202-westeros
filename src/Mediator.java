2ba36512775cea570f5cbc4e0ce3f353afd48676
Jai <jai.chhatre@gmail.com>
2019-05-02 18:34:19 -0700

    setting up mediator pattern classes

---
+++ src/Mediator.java
@@ -0,0 +1,14 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Mediator here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Mediator extends Actor implements IMediator
+{
+    public void setCommanPattern(){
+        System.out.println();
+    }   
+}
